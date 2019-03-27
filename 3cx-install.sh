ed900b33093d1d9e01fa27b07c468530250b8b76
gabyfulchic <gaby.fulchic@ynov.com>
2019-03-27 15:18:07 +0100

    Script replaced by the true one

---
+++ 3cx-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $OSTYPE -e "linux-gnu" ]
+then
+  echo "Type d'os Valide."
+else
+  exit
+fi
+
+# Function d'installation de 3cx
+install3cx()
+{
+  apt install wget -y
+  wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
+  echo "deb http://downloads-global.3cx.com/downloads/debian $1 main" | tee /etc/apt/sources.list.d/3cxpbx.list
+  apt update -y && apt install net-tools -y
+  apt install 3cxpbx
+}
+
+cat /etc/os-release | grep -i "jessie"
+if [ $? -e 0 ]
+then
+  osrelease="jessie"
+  install3cx($osrelease)
+fi
+
+cat /etc/os-release | grep -i "stretch"
+if [ $? -e 0 ]
+then
+  osrelease="stretch"
+  install3cx($osrelease)
+fi
